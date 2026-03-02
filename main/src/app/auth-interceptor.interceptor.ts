826cb72877c97f4571f28b96ca32051afe55c0d5
Harikrishna102024 <tharikrishna.career@gmail.com>
2026-03-02 22:33:03 +0530

    tostr added for expiration tocken

--- main/src/app/auth-interceptor.interceptor.ts
+++ main/src/app/auth-interceptor.interceptor.ts
@@ -1,11 +1,13 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { catchError, throwError } from 'rxjs';
 
 export const authInterceptorInterceptor: HttpInterceptorFn = (req, next) => {
 
   const router = inject(Router);
+  const toastr = inject(ToastrService);
 
   const token = localStorage.getItem('token');
 
@@ -21,6 +23,7 @@ export const authInterceptorInterceptor: HttpInterceptorFn = (req, next) => {
         if (err.status === 401) {
           localStorage.removeItem("token");
           router.navigate(['/login']);
+          toastr.error('Session expired. Please log in again.');
         }
         
         return throwError(() => err);

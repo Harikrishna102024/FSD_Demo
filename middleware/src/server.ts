fefe0f832944ef4a304805d2335c3caa1aeff919
Harikrishna102024 <tharikrishna.career@gmail.com>
2026-03-02 21:07:22 +0530

    clustering

--- middleware/src/server.ts
+++ middleware/src/server.ts
@@ -4,7 +4,19 @@ import cluster from 'cluster';
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+if (cluster.isMaster) {
+
+  const cors = os.cpus().length;
+
+  for (let i = 0; i < cors; i++) {
+    cluster.fork();
+  }
+
+} else {
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+
+}
 

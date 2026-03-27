fd4ba934cefc27d7ea59315f8004968e6ba1ceaf
Harikrishna102024 <tharikrishna.career@gmail.com>
2026-03-27 15:04:48 +0530

    aaaa

--- middleware/src/config/gmail.ts
+++ middleware/src/config/gmail.ts
@@ -1,26 +1,26 @@
 import dotenv from 'dotenv';
-// import nodemailer from 'nodemailer';
-import sgMail from '@sendgrid/mail';
+import nodemailer from 'nodemailer';
+// import sgMail from '@sendgrid/mail';
 
 dotenv.config();
 
+const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST || "smtp.gmail.com",
+    port: Number(process.env.SMTP_PORT) || 465,
+    secure: process.env.SMTP_SECURE === "true",
+    auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+    },
+});
 
-// const transporter = nodemailer.createTransport({
-//     service: 'gmail',
-//     auth: {
-//         user: process.env.MAIL_USER,
-//         pass: process.env.MAIL_PASS
-//     }
-    
-// // })
-
-// export default transporter;
+export default transporter;
 
 
 
-const key = process.env.SENDGRID_API_KEY as string
+// const key = process.env.SENDGRID_API_KEY as string
 
-sgMail.setApiKey(key);
+// sgMail.setApiKey(key);
 
-export default sgMail;
+// export default sgMail;
 
--- middleware/src/services/gmail.service.ts
+++ middleware/src/services/gmail.service.ts
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
-// import transporter from '../config/gmail';
+import transporter from '../config/gmail';
 import finalHtml from './gmail.html';
 import sgMail from '../config/gmail';
 import logger from '../config/winston'
@@ -8,51 +8,58 @@ dotenv.config();
 
 export class GmailService {
 
-    // async sendMail(to: any, subject: any) {
-
-    //     try {
-
-    //         var info = await transporter.sendMail({
-    //             from: process.env.MAIL_USER,
-    //             to: to,
-    //             subject: subject,
-    //             html: subject === 'log' ? finalHtml.log(to) : finalHtml.reg(to)
-    //         })
-
-    //     } catch (err) {
-    //         console.error(" gmail error : ", err);
-    //         return null;
-    //     }
-
-    //     return info;
-
-    // }
-
-
     async sendMail(to: any, subject: any) {
 
         try {
 
-            console.log("🔥 sendMail called");
-            logger.info("🔥 sendMail called")
+            console.log("mail sending.....")
+            logger.info("mail sending.....")
 
-            var info = await sgMail.send({
+            var info = await transporter.sendMail({
+                from: process.env.MAIL_USER,
                 to: to,
-                from: process.env.SENDGRID_MAIL_USER as string,
                 subject: subject,
                 html: subject === 'log' ? finalHtml.log(to) : finalHtml.reg(to)
-            });
-
-            console.log("✅ mail sent");
-            logger.info("✅ mail sent")
+            })
+            console.log("mail sent successfull")
+            logger.info("mail sent successfull")
 
         } catch (err) {
-            console.error("mail error:", err);
-            logger.error("Faild to sent mail")
+            console.error(" gmail error : ", err);
+            logger.error("faild to send mail")
 
             return null;
         }
 
         return info;
+
     }
+
+
+    // async sendMail(to: any, subject: any) {
+
+    //     try {
+
+    //         console.log("🔥 sendMail called");
+    //         logger.info("🔥 sendMail called")
+
+    //         var info = await sgMail.send({
+    //             to: to,
+    //             from: process.env.SENDGRID_MAIL_USER as string,
+    //             subject: subject,
+    //             html: subject === 'log' ? finalHtml.log(to) : finalHtml.reg(to)
+    //         });
+
+    //         console.log("✅ mail sent");
+    //         logger.info("✅ mail sent")
+
+    //     } catch (err) {
+    //         console.error("mail error:", err);
+    //         logger.error("Faild to sent mail")
+
+    //         return null;
+    //     }
+
+    //     return info;
+    // }
 }

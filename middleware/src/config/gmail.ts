1125a7217063c23eb5e4ba64c9586ea0ef1ef703
Harikrishna102024 <tharikrishna.career@gmail.com>
2026-03-27 10:31:24 +0530

    aaaa

--- middleware/src/config/gmail.ts
+++ middleware/src/config/gmail.ts
@@ -2,11 +2,11 @@ import dotenv from 'dotenv';
 import nodemailer from 'nodemailer';
 
 dotenv.config();
-
+const port = Number(process.env.SMTP_PORT)
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
-    port: Number(process.env.SMTP_PORT),
-    secure: false,
+    port: port,
+    secure: port === 465,
     auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS

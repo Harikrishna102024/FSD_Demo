088fb76047c7f9c6a272db229bcccf545b645eae
Harikrishna102024 <tharikrishna.career@gmail.com>
2026-03-29 15:31:48 +0530

    aa

--- middleware/src/app.ts
+++ middleware/src/app.ts
@@ -7,7 +7,8 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:4200', 'https://app-uservoult.onrender.com'],
+  // origin: ['http://localhost:4200', 'https://app-uservoult.onrender.com'],
+  origin: true,
   methods: ['GET','POST','PUT','DELETE','PATCH'],
   allowedHeaders: ['Content-Type','Authorization'],
   credentials: true

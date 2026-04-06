9aad512c1dd6dfc7f6e216947ed3eda86874423b
Harikrishna102024 <tharikrishna.career@gmail.com>
2026-04-07 02:41:03 +0530

    aaaa

--- main/src/app/login/login.component.ts
+++ main/src/app/login/login.component.ts
@@ -1,20 +1,29 @@
-import { Component } from '@angular/core';
+import { Attribute, Component, OnInit } from '@angular/core';
 import { UsedataService } from '../Services/usedata.service';
 import { ToastrService } from 'ngx-toastr';
 import { Router } from '@angular/router';
 import { AppContext } from '../app.context';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
   loading: boolean = false;
   hideText: boolean = true;
+  attemptCount: number = 0;
+  message: any;
+  time: any;
+  interval: any;
 
   constructor(private service: UsedataService, public toastr: ToastrService, private router: Router, public context: AppContext) { }
+
+  ngOnInit() {
+    this.trackRateLimitState()
+  }
 
   checkUserDetails(data: any) {
     this.hideText = false;
@@ -27,7 +36,7 @@ export class LoginComponent {
     }
 
     this.service.checkLoginCredentials(email, password).subscribe((res) => {
-    
+
       if (res && res.success) {
         localStorage.setItem('token', res.auth_token);
         this.loading = false;
@@ -37,11 +46,80 @@ export class LoginComponent {
       }
 
     }, (err) => {
+
       if (err) {
+
+        if (err.status === 429) {
+
+          const retryAfter = err.error.retryAfter;
+          const lockTime = Date.now() + retryAfter * 1000;
+
+          localStorage.setItem("lockTime", lockTime.toString());
+          localStorage.setItem("message", err.error.message);
+
+          this.message = err.error.message;
+          this.trackRateLimitState();
+
+        } else
+          if (err.error.count) {
+
+            this.attemptCount += 1;
+            const maxAttempts = 3;
+
+            if (this.attemptCount < maxAttempts) {
+              this.message = `You have only (${maxAttempts - this.attemptCount}) attempts left`;
+            } else if (this.attemptCount === maxAttempts) {
+              this.message = err.error.message;
+            }
+
+          }
         this.hideText = !this.hideText
         this.loading = false
         this.toastr.error(err.error.message);
       }
     })
+  }
+
+  trackRateLimitState() {
+
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+
+    this.interval = setInterval(() => {
+
+      const lockTime = localStorage.getItem("lockTime");
+      const msg = localStorage.getItem("message");
+
+      if (!lockTime) {
+        clearInterval(this.interval);
+        return;
+      }
+
+      const remainingMs = Number(lockTime) - Date.now();
+
+      if (remainingMs > 0) {
+
+        const totalSeconds = Math.floor(remainingMs / 1000);
+
+        const minutes = Math.floor(totalSeconds / 60);
+        const seconds = totalSeconds % 60;
+
+        this.message = msg;
+
+        this.time = `(${minutes} min ${seconds} sec)`;
+
+      } else {
+
+        this.message = "";
+        this.time = "";
+
+        localStorage.removeItem("lockTime");
+        localStorage.removeItem("message");
+
+        clearInterval(this.interval);
+      }
+
+    }, 1000);
   }
 }

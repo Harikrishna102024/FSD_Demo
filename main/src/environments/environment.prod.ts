7a700a5583f24b1adf0b81aa7e21ce115842e0e2
Harikrishna102024 <tharikrishna.career@gmail.com>
2026-04-13 07:24:01 +0530

    env ui

--- main/src/environments/environment.prod.ts
+++ main/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production :true,
     baseUrl: 'https://uservoult.onrender.com/api',
-    publicBaseUrl: 'https://uservoult.onrender.com/public'
 };
--- main/src/environments/environment.ts
+++ main/src/environments/environment.ts
@@ -2,10 +2,8 @@
 
         production: false,
         baseUrl: 'http://localhost:4000/api',
-        publicBaseUrl: 'http://localhost:4000/public',
 
         // production :true,
         // baseUrl: 'https://uservoult.onrender.com/api'
-        // publicBaseUrl: 'http://localhost:4000/public',
 
     };

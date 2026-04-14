b581f6c5b4452207440cda147ba2f1b1728f4dad
Harikrishna102024 <tharikrishna.career@gmail.com>
2026-04-14 12:41:34 +0530

    mb erroR

--- middleware/src/middleware/uploads.middleware.ts
+++ middleware/src/middleware/uploads.middleware.ts
@@ -6,13 +6,14 @@ export const fileUpload = (req: Request, res: Response, next: NextFunction) => {
     upload.single('profile')(req, res, (err) => {
 
         if (err) {
+            
             (req as any).fileError = {
                 field: 'profile',
                 message: err.code === 'LIMIT_FILE_SIZE'? 'File size should be less than 2MB': 'Invalid file upload'
             };
-        }
 
-        if (!req.file) {
+        } else if (!req.file) {
+
             (req as any).fileError = {
                 field: 'profile',
                 message: 'Profile image is required'

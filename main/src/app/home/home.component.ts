4883d7c4753e444f2a22cffe173dc4444e5c2d90
Harikrishna102024 <tharikrishna.career@gmail.com>
2026-04-17 16:22:54 +0530

    dark header and home

--- main/src/app/home/home.component.ts
+++ main/src/app/home/home.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { AppContext } from '../app.context';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss', './home.component.dark.scss']
 })
 export class HomeComponent {
+
+  constructor(public context: AppContext) {}
 
 }

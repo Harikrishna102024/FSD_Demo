eeb784d862fa5c143ed25c01a0811854768eecfb
Harikrishna102024 <tharikrishna.career@gmail.com>
2026-04-24 14:46:48 +0530

    qwdwd

--- middleware/src/config/db.ts
+++ middleware/src/config/db.ts
@@ -4,18 +4,6 @@ import { Sequelize } from 'sequelize';
 
 dotenv.config();
 
-// const pool = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   port: Number(process.env.DB_PORT)
-// });
-
-// export default pool;
-
-
-
 //sequlize
 const sequelize = new Sequelize(
   process.env.DB_NAME as string,
